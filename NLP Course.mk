c3bd9a4826578138c62fc16b34909863def4e55a
Mohamad <33785330+lpln25@users.noreply.github.com>
2020-07-20 23:55:35 +0430

    NLP
    initial first

---
+++ NLP Course.mk
@@ -0,0 +1,20 @@
+# NLP-Course
+Natural language processing course
+
+### Content list
+1. Introduction
+2. Linear regression with one variable - Model representation
+3. Linear Algebra review (optional) - Matrices and Vectors
+4. Linear Regression with multiple variables - Multiple features
+5. Ovtave Tutorial - Basic operation - Basic operations
+6. Logistic Regression - classification
+7. --
+8. Neural Networks - Non-Linear hypotheses
+9. Neural Network - Cost function
+10. Advice for applying machine learning - Deciding what to try next
+11. Machine learning system design - Prioritzing what to work on: Spam classification example
+12. Support Vector Machine - Optimization objective
+13. Clustering - Unsupervised learning introduction
+14. Dimensionality Reduction - Motivation I: Data compression
+15. Anomaly detection - Problem motivation
+16. Recommender systems - problem formulation
